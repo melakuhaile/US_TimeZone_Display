9684c3bcf2bcdd594af172846a3009a0961a67dc
melakusejamo <melakusejam@gmail.com>
2017-12-06 12:41:17 -0500

    event driven different parts of US current time display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  time = new Date();
+  TimeZoneSelected = null;
+
+  getDate(timezone) {
+    this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+    }
+    this.TimeZoneSelected = timezone;
+  }
+
 }
